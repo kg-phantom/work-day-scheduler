ab00bc7d77b57e55ac966b013297a95fc3ea31cb
Kyrsten <ksurbayan@gmail.com>
2021-09-10 01:28:48 -0800

    save button pushes event description to corresponding index in events array

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,40 @@
-var today = moment().format("dddd, MMMM DD");
-$("#currentDay").append("<p>").text(today);
+function auditDay() {
+    var today = moment().format("dddd, MMMM Do");
+    $("#currentDay").append("<p>").text(today);
+};
+
+var events = JSON.parse(localStorage.getItem("saved events"));
+
+if(!events) {
+    events = ["", "", "", "", "", "", "", "", ""];
+}
+
+function auditEvents() {
+    console.log("hi");
+};
+
+function loadEvents() {
+    for(var i = 0; i < events.length; i++) {
+        var eventId = JSON.parse($("i").attr("id")) + i;
+        console.log(eventId);
+    }
+};
+
+$("i").on("click", function() {
+    var eventDescription = $(this).parents(".row").find("textarea").val();
+    
+    events.splice($(this).attr("id"), 1, eventDescription);
+    localStorage.setItem("saved events", JSON.stringify(events));
+})
+
+// $("textarea").each(function() {
+//     var i = 0;
+//     $(this).text(events[i]);
+//     i++;
+// })
+
+loadEvents();
+
+setInterval(auditDay(), (1000 * 60 * 60));
+
+setInterval(auditEvents(), (1000 * 60 * 30));
